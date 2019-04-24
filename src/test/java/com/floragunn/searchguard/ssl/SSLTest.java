ac66740e1944a401a76f7a0a15164ccb9476764a
floragunn <dev@search-guard.com>
2019-04-24 15:54:33 +0200

    Fix SSL unit tests

--- src/test/java/com/floragunn/searchguard/ssl/SSLTest.java
+++ src/test/java/com/floragunn/searchguard/ssl/SSLTest.java
@@ -605,10 +605,13 @@ public class SSLTest extends SingleClusterTest {
 
         setupSslOnlyMode(settings);
         
-        RestHelper rh = restHelper();
+        RestHelper rh = nonSslRestHelper();
 
         final Settings tcSettings = Settings.builder().put("cluster.name", clusterInfo.clustername).put("path.home", ".")
                 .put("node.name", "client_node_" + new Random().nextInt())
+                .put("node.data", false)
+                .put("node.master", false)
+                .put("node.ingest", false)
                 .put("discovery.initial_state_timeout","8s")
                 .putList("discovery.zen.ping.unicast.hosts", clusterInfo.nodeHost+":"+clusterInfo.nodePort)
                 .put(settings)// -----
